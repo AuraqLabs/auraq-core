904b8ec387a5437bcc70c5c7b7d869f1aea1ff71
KinuCyber <kinu.cyber@proton.me>
2026-03-12 16:48:13 +0500

    Trying out COBE (cobe.vercel.app) vendor

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,5 +1,7 @@
 import { initPanning } from 'https://cdn.auraq.org/modules/panning/panning.init.js';
+import { initGlobe } from 'https://dev.auraq.org/vendor/cobe/cobe.init.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initPanning();
+  initGlobe();
 });
