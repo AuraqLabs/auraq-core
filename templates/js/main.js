398c342d762e9f2c00e55a8fd4e8f1d125548d2d
KinuCyber <kinu.cyber@proton.me>
2026-03-14 22:11:04 +0500

    Updated main.js to test sectionMap

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,7 +1,9 @@
 import { initPanning } from 'https://cdn.auraq.org/modules/panning/panning.init.js';
 import { initGlobe } from 'https://dev.auraq.org/vendor/cobe/cobe.init.js';
+import { initSectionMap } from 'https://dev.auraq.org/modules/sectionMap/sectionMap.init.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initPanning();
   initGlobe();
+  initSectionMap();
 });
