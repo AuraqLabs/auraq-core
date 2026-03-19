6c760f6e56a786f0ed0039065c31aa26f5a0b06a
KinuCyber <kinu@kinu.uk>
2026-03-19 21:58:50 +0500

    Updated index.html and main.js to add skillTree

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,9 +1,11 @@
 import { initPanning } from 'https://cdn.auraq.org/modules/panning/panning.init.js';
 import { initGlobe } from 'https://dev.auraq.org/vendor/cobe/cobe.init.js';
 import { initSectionMap } from 'https://dev.auraq.org/modules/sectionMap/sectionMap.init.js';
+import { initSkillTree } from './skillTree/skillTree.init.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initPanning();
   initGlobe();
   initSectionMap();
+  initSkillTree();
 });
