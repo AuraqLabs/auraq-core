5f79d8847afaf58f895906c9839be0c7cf7e4e0f
KinuCyber <kinu@kinu.uk>
2026-03-28 11:48:20 +0500

    Updated sectionMap URI

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,6 +1,6 @@
 import { initPanning } from 'https://cdn.auraq.org/modules/panning/panning.init.js';
 import { initGlobe } from 'https://dev.auraq.org/vendor/cobe/cobe.init.js';
-import { initSectionMap } from './sectionMap/sectionMap.init.js';
+import { initSectionMap } from 'http://192.168.0.218:8081/modules/sectionMap/sectionMap.init.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initPanning();
