72caf0eebaf9919dd97849880eedfa429c71fb68
KinuCyber <kinu@kinu.uk>
2026-03-31 00:12:24 +0500

    From http to https

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,7 +1,7 @@
 import { initPanning } from 'https://cdn.auraq.org/modules/panning/panning.init.js';
 import { initGlobe } from 'https://dev.auraq.org/vendor/cobe/cobe.init.js';
 import { initSectionMap } from 'https://dev.auraq.org/modules/sectionMap/sectionMap.init.js';
-import { initSkillTree } from 'http://dev.auraq.org/modules/skillTree/skillTree.init.js';
+import { initSkillTree } from 'https://dev.auraq.org/modules/skillTree/skillTree.init.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initGlobe();
